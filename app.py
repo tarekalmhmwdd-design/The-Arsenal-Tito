c9bdf4a1e5f00f52df6bd4af0dd3ea709748744f
tarekalmhmwdd-design <tarek.almhmwdd@gmail.com>
2026-02-22 01:47:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,4 @@ app = Flask(__name__)
 def home():
     return "Arsenal is live 🔥"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__": app.run(host="0.0.0.0", 
